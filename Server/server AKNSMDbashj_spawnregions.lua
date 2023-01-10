3a3a713e264a5fd438c44e21814b0c232da22d6e
MVP <sandreoli@practia.uy>
2023-01-10 10:49:48 -0300

    Primer subida de guardado de partida

---
+++ Server/server AKNSMDbashj_spawnregions.lua
@@ -0,0 +1,14 @@
+function SpawnRegions()
+	return {
+		{ name = "Muldraugh, KY", file = "media/maps/Muldraugh, KY/spawnpoints.lua" },
+		{ name = "Riverside, KY", file = "media/maps/Riverside, KY/spawnpoints.lua" },
+		{ name = "Rosewood, KY", file = "media/maps/Rosewood, KY/spawnpoints.lua" },
+		{ name = "West Point, KY", file = "media/maps/West Point, KY/spawnpoints.lua" },
+		{ name = "west_point_expansion", file = "media/maps/west_point_expansion/spawnpoints.lua" },
+		{ name = "SLEOSpawn", file = "media/maps/SLEOSpawn/spawnpoints.lua" },
+		{ name = "RavenCreek", file = "media/maps/RavenCreek/spawnpoints.lua" },
+		{ name = "Pitstop", file = "media/maps/Pitstop/spawnpoints.lua" },
+		{ name = "Grapeseed", file = "media/maps/Grapeseed/spawnpoints.lua" },
+		{ name = "Blackwood", file = "media/maps/Blackwood/spawnpoints.lua" },
+	}
+end
